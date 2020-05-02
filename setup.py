d73126e927f80df1a98181ef9e65bcc072ec7c49
James <james.walker.nz@me.com>
2020-05-02 12:33:48 +1200

    Adjusted setup.py to use an ini file for entry points.

--- setup.py
+++ setup.py
@@ -3,26 +3,22 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("entry_points.ini", "r") as fh:
+    entry_points = fh.read()
+
 setup(
     name="bf20_mill",
     version="0.0.1",
-    author="John Doe",
-    author_email="<doe.john@example.com>",
+    author="James Walker",
+    author_email="james.walker.nz@me.com",
     description="bf20_mill - A QtPyVCP based Virtual Control Panel for LinuxCNC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/USERNAME/REPO",
-    download_url="https://github.com/USERNAME/REPO/tarball/master",
+    url="https://github.com/joco-nz/bf20_mill",
+    download_url="https://github.com/joco-nz/bf20_mill/tarball/master",
     packages=find_packages(),
     include_package_data=True,
-    entry_points={
-        'gui_scripts': [
-            'bf20_mill=bf20_mill:main',
-        ],
-        'qtpyvcp.vcp': [
-            'bf20_mill=bf20_mill',
-        ],
-    },
+    entry_points=entry_points,
     install_requires=[
        'qtpyvcp',
     ],

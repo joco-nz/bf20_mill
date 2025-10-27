fff201a723bde94cbfe00dc43765a8bd652f312b
James <james@snaggingpixels.com>
2025-10-27 19:19:21 +1300

    Additional fixes for deb build so install works

--- build_deb.sh
+++ build_deb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #make sure build deps are in place
-sudo apt install dpkg-dev
-sudo apt install devscripts build-essential lintian
-sudo apt install dh-python
-sudo apt install python3-build
-sudo apt install pybuild-plugin-pyproject
-sudo apt install python3-poetry
+#sudo apt install dpkg-dev
+#sudo apt install devscripts build-essential lintian
+#sudo apt install dh-python
+#sudo apt install python3-build
+#sudo apt install pybuild-plugin-pyproject
+#sudo apt install python3-poetry
 
 debuild -ePATH  -b -uc -us
 
